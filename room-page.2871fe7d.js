688a91ba95f771182628b0b3706ff61d8b60f08f
andriitomilov <andriitomilov@ukr.net>
2022-12-13 14:51:21 +0000

    Deploying to gh-pages from @ andriitomilov/dream-team-14@4d810a4b1fff7c8a38883d0be9473d643c8df500 🚀

---
+++ room-page.2871fe7d.js
@@ -0,0 +1,2 @@
+new Swiper(".hero-slider",{navigation:{nextEl:"#hero-slider-next",prevEl:"#hero-slider-prev"},autoHeight:!0,slideToClickedSlide:!0,grabCursor:!0,keyboard:{enabled:!0,onlyInViewport:!0},slidesPerView:1,watchOverflow:!0,centeredSlides:!0,loop:!0,effect:"fade",simulateTouch:!0,touchRatio:1});
+//# sourceMappingURL=room-page.2871fe7d.js.map
